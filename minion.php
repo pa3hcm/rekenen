c89858339cf2824693a14196eadd8eb7b131cd57
pa3hcm <ernest@utreg.net>
2016-11-18 19:37:07 +0100

    Minion gebouwd

---
+++ minion.php
@@ -0,0 +1,20 @@
+<?php
+
+print "Content-Type: text/plain\n\n";
+
+function IsPrime($n) {  
+	for($x=2; $x<$n; $x++) {  
+		if($n %$x ==0) { return 0; }  
+	}
+	return 1;  
+}
+
+$num = 0;
+while ($num < 10000) {
+	$num = file_get_contents('http://rekenen-rekenen.oc.utreg.net/api.php');
+	if (IsPrime($num) && $num>0) {
+		print "$num is a prime number\n";
+	} else {
+		print "$num is NOT a prime number\n";
+	}
+}
