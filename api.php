a76005af79bff7e47f7cbfdeb589187b1eb43246
pa3hcm <ernest@utreg.net>
2016-11-18 19:06:27 +0100

    Initiele versie

---
+++ api.php
@@ -0,0 +1,26 @@
+<?php
+print "Content-Type: text/plain\n\n";
+
+$file = '/tmp/postcode.txt';
+
+if (file_exists($file)) {
+	$myfile = fopen($file, 'r');
+	$num = intval(fgets($myfile));
+	fclose($myfile);
+	$num++;
+	if ($num < 10000) {
+		$myfile = fopen($file, 'w');
+		fwrite ($myfile, $num);
+		fclose($myfile);
+	} else {
+		$num = 0;
+	}
+} else {
+	$num = 1000;
+	$myfile = fopen($file, 'w');
+	fputs ($myfile, $num);
+	fclose($myfile);
+}
+
+print $num;
+?>
---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+print "Content-Type: text/html\n\n";
+
+$file = '/tmp/postcode.txt';
+
+if (file_exists($file)) {
+	$myfile = fopen($file, 'r');
+	$num = intval(fgets($myfile));
+	fclose($myfile);
+} else {
+	$num = 0;
+}
+?>
+<html>
+<head>
+	<title>Rekenen</title>
+	<meta http-equiv="refresh" content="5" />
+</head>
+
+<body>
+<h1>Rekenen</h1>
+<p><?php print $num; ?></p>
+</body>
+
+</html>
